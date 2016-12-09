cabc7471742c2a67ff395e68bb05bd2ba9d57f8e
Luke Campagnola <luke.campagnola@gmail.com>
2016-12-09 11:03:25 -0800

    minor edits

--- mies_nwb_viewer.py
+++ mies_nwb_viewer.py
@@ -1,3 +1,4 @@
+import sys
 import acq4
 from neuroanalysis.nwb_viewer import MiesNwbViewer, MiesNwb
 acq4.pyqtgraph.dbg()
@@ -6,6 +7,7 @@ m = acq4.Manager.Manager(argv=['-D', '-n', '-m', 'Data Manager'])
 dm = m.getModule('Data Manager')
 v = MiesNwbViewer()
 v.show()
+v.setWindowTitle('NWB Viewer')
 
 def load_from_dm():
     v.set_nwb(MiesNwb(m.currentFile.name()))
@@ -13,3 +15,7 @@ def load_from_dm():
 btn = acq4.pyqtgraph.Qt.QtGui.QPushButton('load from data manager')
 v.vsplit.insertWidget(0, btn)
 btn.clicked.connect(load_from_dm)
+
+
+if sys.flags.interactive == 0:
+    acq4.pyqtgraph.QtGui.QApplication.exec_()
