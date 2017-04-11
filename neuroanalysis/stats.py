8f6a279b9ece341729dc3c3366fb2ff9b3b85897
Luke Campagnola <lukec@alleninstitute.org>
2017-04-11 16:26:15 -0700

    add sliding binomial for connectivity characterization

--- neuroanalysis/stats.py
+++ neuroanalysis/stats.py
@@ -24,6 +24,70 @@ def binomial_ci(p, n, alpha=0.05 ):
     return scipy.optimize.bisect(lower_fn, 0, 1), scipy.optimize.bisect(upper_fn, 0, 1)
 
 
+def binomial_sliding_window(x, success, window, spacing=None, alpha=0.05):
+    """Given a set of success/failure events occurring at different positions,
+    measure the probability of success versus position using a sliding window.
+    Also generate confidence intervals on the probability of a binomial
+    distribution.
+    
+    This function is used primarily to measure the probability of synaptic
+    connection as it varies with the distance between cells. 
+
+    Parameters
+    ----------
+    x : float array
+        X values of each observation. For cell-cell connectivity, this is the
+        distance between cells.
+    success : bool array
+        True/False indicating success or failure for each observation
+    window : float
+        The width of the sliding window within which success probability and
+        confidence intervals are computed
+    spacing : float
+        Distance to advance window for each step.
+    alpha : float
+        Width of confidence interval (alpha=0.05 gives 95% ci)
+    
+    Returns
+    -------
+    xvals : array
+        Center x value of each window step
+    proportion : array
+        Proportion of successful trials in each window step
+    lower : array
+        Lower binomial confidence interval value at each window step
+    upper : array
+        Upper binomial confidence interval value at each window step
+    """
+    if spacing is None:
+        spacing = window / 4.0
+        
+    xvals = np.arange(window / 2.0, 500e-6, spacing)
+    upper = []
+    lower = []
+    prop = []
+    ci_xvals = []
+    for x1 in xvals:
+        minx = x1 - window / 2.0
+        maxx = x1 + window / 2.0
+        # select points inside this window
+        mask = (x >= minx) & (x <= maxx)
+        pts_in_window = success[mask]
+        # compute stats for window
+        n_probed = pts_in_window.shape[0]
+        n_conn = pts_in_window.sum()
+        if n_probed > 0:
+            #prop.append(np.nan)
+        #else:
+            prop.append(n_conn / n_probed)
+            ci = binomial_ci(n_conn, n_probed, alpha=alpha)
+            lower.append(ci[0])
+            upper.append(ci[1])
+            ci_xvals.append(x1)
+    
+    return ci_xvals, prop, lower, upper
+
+
 def ragged_mean(arrays, method='pad'):
     """Return the mean of a list of arrays, where each array may have 
     different length.
