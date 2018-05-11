4266f2580addde31732da4df3c7d0490eaa01975
Luke Campagnola <lukec@alleninstitute.org>
2018-05-11 14:15:57 -0700

    commenting

--- neuroanalysis/tests/test_stimuli.py
+++ neuroanalysis/tests/test_stimuli.py
@@ -21,15 +21,16 @@ def test_stimuli():
     assert sp1.duration == 0.2
     assert sp1.amplitude == 10
 
+    # check evaluation of pulse waveform
     sp1_eval = sp1.eval(n_pts=1000, dt=0.001)
     assert sp1_eval.dt == 0.001
     sp1_data = sp1_eval.data
     assert sp1_data.shape == (1000,)
-
     test_data = np.zeros(1000)
     test_data[100:300] = 10
     assert np.all(sp1_data == test_data)
 
+    # test parent/child logic
     sp1.parent = s1
     assert sp1.parent is s1
     assert s1.items == (sp1,)
@@ -52,6 +53,7 @@ def test_stimuli():
     assert sp2.parent is s1
     assert s1.items == (sp2,)
     
+    # more parent / child logic testing
     s1.insert_item(1, sp1)
     assert sp1.parent is s1
     assert s1.items == (sp2, sp1)
@@ -61,11 +63,11 @@ def test_stimuli():
     assert sp1.parent is s1
     assert s1.items == (sp1, sp2)
 
+    # test waveform eval with two pulses
     s1_data = s1.eval(n_pts=1000, dt=0.001).data
     test_data[200:400] -= 10
     assert np.all(sp1_data == test_data)
     
-
     # test a pulse train
     pt1 = stimuli.SquarePulseTrain(start_time=0.5, n_pulses=3, pulse_duration=0.02, interval=0.1, amplitude=1.0, parent=s1)
     assert p1.items == (sp1, sp2, pt1)
@@ -73,5 +75,3 @@ def test_stimuli():
     test_data[600:620] += 1
     test_data[700:720] += 1
     assert np.all(sp1_data == test_data)
-
-
